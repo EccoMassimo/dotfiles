008de51d052d3554ec30c76483c790b1ce443a67
Max Amundsen <massimo@cdrateline.com>
2019-11-11 19:53:44 -0500

    modified:   .Xresources 	modified:   .config/i3/config 	modified:   user.js

--- user.js
+++ user.js
@@ -1,38 +1,34 @@
 //Massimo's user.js
 
 //General
-pref("general.warnOnAboutConfig", false);
-pref("browser.warnOnQuit", false);
-pref("browser.preferences.inContent",false);
-pref("general.useragent.override", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:24.0) Gecko/20100101 Firefox/60.0");
+user_pref("general.warnOnAboutConfig", false);
+user_pref("browser.warnOnQuit", false);
+user_pref("browser.user_preferences.inContent",false);
+user_pref("general.useragent.override", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:24.0) Gecko/20100101 Firefox/60.0");
+user_pref("browser.urlbar.autocomplete.enabled", false);
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("browser.tabs.loadInBackground", false);
 
 //Appearance
-pref("font.internaluseonly.changed", true);
-pref("font.name.monospace.x-western", "DejaVu Sans Mono");
-pref("font.name.sans-serif.x-western", "DejaVu Sans");
-pref("font.name.serif.x-western", "DejaVu Serif");
-pref("font.size.fixed.x-western", 14);
-pref("font.size.variable.x-western", 14);
-pref("browser.urlbar.placeholderName", "SuccSuccGo");
-pref("browser.startup.homepage", "about:blank");
-pref("browser.newtabpage.enabled", false);
-pref("dom.webnotifications.enabled",false); 
-pref("dom.webnotifications.serviceworker.enabled",false); 
-pref("dom.push.enabled",false); 
-pref("widget.chrome.allow-gtk-dark-theme", true);
-pref("widget.content.allow-gtk-dark-theme",true);
-pref("general.smoothScroll", false);
-pref("browser.newtabpage.activity-stream.showTopSites", false);
-pref("browser.newtabpage.activity-stream.prerender", false);
-pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
-pref("browser.newtabpage.activity-stream.showTopSites", false);
-
-pref("extensions.blocklist.enabled", false);
-pref("services.sync.prefs.sync.security.OCSP.enabled", false);
-pref("security.OCSP.GET.enabled", false);
-pref("security.OCSP.require", false);
-pref("security.OCSP.enabled", 0);
-pref("geo.enabled", false);
+user_pref("font.internaluseonly.changed", true);
+user_pref("font.name.monospace.x-western", "DejaVu Sans Mono");
+user_pref("font.name.sans-serif.x-western", "Helv");
+user_pref("font.name.serif.x-western", "Helv");
+user_pref("font.size.fixed.x-western", 11);
+user_pref("font.size.variable.x-western", 14);
+user_pref("browser.urlbar.placeholderName", "SuccSuccGo");
+user_pref("browser.startup.homepage", "about:blank");
+user_pref("browser.newtabpage.enabled", false);
+user_pref("dom.webnotifications.enabled",false); 
+user_pref("dom.webnotifications.serviceworker.enabled",false); 
+user_pref("dom.push.enabled",false); 
+user_pref("widget.chrome.allow-gtk-dark-theme", true);
+user_pref("widget.content.allow-gtk-dark-theme",true);
+user_pref("general.smoothScroll", false);
+user_pref("browser.newtabpage.activity-stream.showTopSites", false);
+user_pref("browser.newtabpage.activity-stream.prerender", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
+user_pref("browser.newtabpage.activity-stream.showTopSites", false);
 
 
 //Clear history settings
@@ -43,30 +39,39 @@ user_pref("privacy.firstparty.isolate", false);
 user_pref("privacy.history.custom", true);
 user_pref("privacy.sanitize.pending", "[{\"id\":\"shutdown\",\"itemsToClear\":[\"cache\",\"history\",\"formdata\",\"downloads\",\"siteSettings\"],\"options\":{}}]");
 user_pref("privacy.sanitize.sanitizeOnShutdown", true);
-user_pref("browser.sessionstore.max_tabs_undo", = 0);
+user_pref("browser.sessionstore.max_tabs_undo",  0);
 
 //Anti-Botnet
-user_pref("privacy.resistFingerprinting", = true);
-user_pref("privacy.trackingprotection.fingerprinting.enabled", = true);
-user_pref("privacy.trackingprotection.cryptomining.enabled", = true);
-user_pref("privacy.trackingprotection.enabled", = true);
-user_pref("browser.send_pings", = false);
-user_pref("browser.urlbar.speculativeConnect.enabled", = false);
-user_pref("dom.event.clipboardevents.enabled", = false);
-user_pref("media.eme.enabled", = false);
-user_pref("media.gmp-widevinecdm.enabled", = false);
-user_pref("media.navigator.enabled", = false);
-user_pref("network.cookie.cookieBehavior", = 1);
-user_pref("network.http.referer.XOriginPolicy", = 2);
-user_pref("webgl.disabled", = true);
-user_pref("browser.sessionstore.privacy_level", = 0); //0=Store All 1=Store Data for HTTP only 2=Never Store Session Data
-user_pref("network.IDN_show_punycode", = true);
+user_pref("privacy.resistFingerprinting",  true);
+user_pref("privacy.trackingprotection.fingerprinting.enabled", true);
+user_pref("privacy.trackingprotection.cryptomining.enabled", true);
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("browser.send_pings", false);
+user_pref("browser.urlbar.speculativeConnect.enabled", false);
+user_pref("dom.event.clipboardevents.enabled", false);
+user_pref("media.eme.enabled",  false);
+user_pref("media.gmp-widevinecdm.enabled", false);
+user_pref("media.navigator.enabled",  false);
+user_pref("network.cookie.cookieBehavior",  1);
+user_pref("network.http.referer.XOriginPolicy", 2);
+user_pref("webgl.disabled",  true);
+user_pref("browser.sessionstore.privacy_level",  0); //0Store All 1Store Data for HTTP only 2Never Store Session Data
+user_pref("network.IDN_show_punycode",  true);
+user_pref("browser.safebrowsing.phishing.enabled", false);
+user_pref("browser.safebrowsing.malware.enabled", false);
+user_pref("extensions.blocklist.enabled", false);
+user_pref("services.sync.user_prefs.sync.security.OCSP.enabled", false);
+user_pref("security.OCSP.GET.enabled", false);
+user_pref("security.OCSP.require", false);
+user_pref("security.OCSP.enabled", 0);
+user_pref("geo.enabled", false);
+user_pref("privacy.donottrackheader.enabled", true);
 
 //Speeding it up
-pref("network.http.pipelining", true);
-pref("network.http.proxy.pipelining", true);
-pref("network.http.pipelining.maxrequests", 10);
-pref("nglayout.initialpaint.delay", 0);
+user_pref("network.http.pipelining", true);
+user_pref("network.http.proxy.pipelining", true);
+user_pref("network.http.pipelining.maxrequests", 10);
+user_pref("nglayout.initialpaint.delay", 0);
 
 
 
