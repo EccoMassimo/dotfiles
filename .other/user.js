e5734274a25466af463e0fa33befc5cb00607606
Max Amundsen <massimo@cdrateline.com>
2019-11-14 19:04:54 -0500

    modified:   .other/user.js

--- .other/user.js
+++ .other/user.js
@@ -8,6 +8,7 @@ user_pref("general.useragent.override", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64;
 user_pref("browser.urlbar.autocomplete.enabled", false);
 user_pref("browser.tabs.warnOnClose", false);
 user_pref("browser.tabs.loadInBackground", false);
+user_pref("browser.urlbar.searchSuggestionsChoice", false);
 
 //Appearance
 user_pref("font.internaluseonly.changed", true);
@@ -29,13 +30,14 @@ user_pref("browser.newtabpage.activity-stream.showTopSites", false);
 user_pref("browser.newtabpage.activity-stream.prerender", false);
 user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
 user_pref("browser.newtabpage.activity-stream.showTopSites", false);
-
+user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"personal-bookmarks\",\"urlbar-container\",\"downloads-button\",\"jid1-ktlzuoiikvffew_jetpack-browser-action\",\"tortm-browser-button_jeremybenthum-browser-action\",\"tprb_addon_searxes_danwin1210_me-browser-action\",\"https-everywhere_eff_org-browser-action\",\"ublock0_raymondhill_net-browser-action\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[]},\"seen\":[\"developer-button\",\"jid1-ktlzuoiikvffew_jetpack-browser-action\",\"tortm-browser-button_jeremybenthum-browser-action\",\"tprb_addon_searxes_danwin1210_me-browser-action\",\"https-everywhere_eff_org-browser-action\",\"ublock0_raymondhill_net-browser-action\"],\"dirtyAreaCache\":[\"PersonalToolbar\",\"nav-bar\",\"TabsToolbar\",\"toolbar-menubar\"],\"currentVersion\":16,\"newElementCount\":4}");
+user_pref("browser.uidensity", 1);
 
 //Clear history settings
 user_pref("privacy.clearOnShutdown.cookies", false);
 user_pref("privacy.clearOnShutdown.sessions", false);
 user_pref("privacy.clearOnShutdown.siteSettings", true);
-user_pref("privacy.firstparty.isolate", false);
+user_pref("privacy.firstparty.isolate", true);
 user_pref("privacy.history.custom", true);
 user_pref("privacy.sanitize.pending", "[{\"id\":\"shutdown\",\"itemsToClear\":[\"cache\",\"history\",\"formdata\",\"downloads\",\"siteSettings\"],\"options\":{}}]");
 user_pref("privacy.sanitize.sanitizeOnShutdown", true);
@@ -53,7 +55,8 @@ user_pref("media.eme.enabled",  false);
 user_pref("media.gmp-widevinecdm.enabled", false);
 user_pref("media.navigator.enabled",  false);
 user_pref("network.cookie.cookieBehavior",  1);
-user_pref("network.http.referer.XOriginPolicy", 0); //this breaks captchas if set above 0
+user_pref("network.http.referer.XOriginPolicy", 0);
+user_pref("network.http.referer.XOriginTrimmingPolicy", 0);
 user_pref("webgl.disabled",  true);
 user_pref("browser.sessionstore.privacy_level",  0); //0Store All 1Store Data for HTTP only 2Never Store Session Data
 user_pref("network.IDN_show_punycode",  true);
