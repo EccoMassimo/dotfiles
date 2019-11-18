c4787e391830a7fb0936539e227daf95e6f43e56
Max Amundsen <massimo@cdrateline.com>
2019-11-17 22:38:40 -0500

    modified:   .config/i3/config 	modified:   .config/qutebrowser/config.py 	modified:   .config/qutebrowser/quickmarks 	modified:   .local/share/qutebrowser/jblock-whitelist

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -8,14 +8,14 @@ c.content.geolocation = False
 c.scrolling.bar = 'always'
 c.tabs.background = True
 c.zoom.default = '100%'
-c.auto_save.session = True
+c.auto_save.session = False
 c.backend = 'webengine'
 c.url.default_page = 'about:blank'
 c.url.start_pages = 'about:blank'
 c.completion.web_history.max_items = 100
 c.statusbar.position = 'bottom'
 c.tabs.position = 'top'
-c.content.host_blocking.enabled = False
+c.content.host_blocking.enabled = True
 c.content.headers.do_not_track = True
 #c.content.headers.user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/601.2.7 (KHTML, like Gecko) Version/9.0.1 Safari/601.2.7'
     
