f352067ad556bc9417dcdb533022063dce429560
massimo <massimo@sasha.localdomain>
2020-01-11 09:42:37 -0500

    modified:   .scripts/mcaftacip.sh 	modified:   .scripts/screenfetch.sh

--- .scripts/mcaftacip.sh
+++ .scripts/mcaftacip.sh
@@ -8,22 +8,6 @@ welcomemsg() { \
 	whiptail --title "Welcome!" --msgbox "Thank you for downloading Max's Catastrophically Automatic File Transfer and Customized Installation Protocol. This script will install all the programs and utils you need to get Work™ done. \\n\\n-Max" 12 60
 	}
 
-licensemsg() { \
-	whiptail --title "WTFPLv2" --msgbox "            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-                    Version 2, December 2004
-
- Copyright (C) 2019 Max Amundsen <max@maxamundsen.xyz>
-
- Everyone is permitted to copy and distribute verbatim or modified
- copies of this license document, and changing it is allowed as long
- as the name is changed.
-
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-
-  0. You just DO WHAT THE FUCK YOU WANT TO." 20 75
-	}
-
 choosedotfiles() { \
 	edition="$(whiptail --title "Dotfiles" --menu "Select dotfiles (config files) to import for the user:" 12 70 3 0 "Max's default configuration (recommended)." 1 "Don't import dotfiles (scary mode)." 3>&1 1>&2 2>&3 3>&1)" || error "User quit the installer."
 	}
@@ -39,8 +23,10 @@ getuser() { \
 installdotfiles(){ \
 if echo "$edition" | grep '0' >/dev/null 2>&1; then
 	cd /home/"$username"
-	git clone "https://github.com/eccomassimo/dotfiles.git"
-	mv /home/"$username"/dotfiles/.* /home/"$username"/* /home/"$username"/
+	git clone https://github.com/eccomassimo/dotfiles.git
+	#cd /home/"$username"/dotfiles
+	mv /home/"$username"/dotfiles/* /home/"$username"/dotfiles/.* /home/"$username"/
+	cd /home/"$username"
 	rm -rf dotfiles/
 	fi
 
@@ -56,7 +42,10 @@ chooseprograms(){ \
 installprograms(){ \
 if echo "$programs" | grep '0' >/dev/null 2>&1; then
 	sudo dpkg --add-architecture i386
-	sudo apt install cowsay cmatrix libreoffice audacity irssi lynx htop neofetch ttf-mscorefonts-installer steam lolcat figlet screenkey obs-studio youtube-dl lutris gimp
+	sudo apt install cowsay cmatrix libreoffice audacity irssi lynx htop ttf-mscorefonts-installer steam lolcat figlet screenkey obs-studio youtube-dl lutris gimp
+	cd /home/"$username"
+	mkdir .programs
+	installunchromium
 	fi
 
 if echo "$programs" | grep '1' >/dev/null 2>&1; then
@@ -64,42 +53,33 @@ if echo "$programs" | grep '1' >/dev/null 2>&1; then
 	fi
 }
 
-choosesudopasswd(){ \
-	sudopasswd="$(whiptail --title "Require a password?" --menu "Should $username input a password for root commands?" 10 80 2 0 "Nope" 1 "Yep" 3>&1 1>&2 2>&3 3>&1)" || error "User quit the installer."		
+installunchromium(){ \
+	cd /home/"$username"/.programs
+	wget https://github.com/Eloston/ungoogled-chromium-binaries/releases/download/78.0.3904.108-2.1/ungoogled-chromium_78.0.3904.108-2.1_linux.tar.xz
+	tar -xf ungoogled-chromium_78.0.3904.108-2.1_linux.tar.xz
+	rm ungoogled-chromium_78.0.3904.108-2.1_linux.tar.xz
+	sleep .5
+	cd /home/"$username"/.programs/ungoogled-chromium_78.0.3904.108-2.1_linux
+	mv chrome_sandbox chrome-sandbox
+	sudo chmod 4755 /home/"$username"/.programs/ungoogled-chromium_78.0.3904.108-2.1_linux/chrome-sandbox
+	sudo chown root /home/"$username"/.programs/ungoogled-chromium_78.0.3904.108-2.1_linux/chrome-sandbox
+	
+
 }
-
-setsudopasswd(){ \
-if echo "$sudopasswd" | grep '0' >/dev/null 2>&1; then
-	echo "$username ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-	fi
-
-if echo "$sudopasswd" | grep '1' >/dev/null 2>&1; then
-	echo "Will continue without passwd"
-	fi
-}
-
-
 
 error() { clear; printf "Installation could not complete:\\n%s\\n" "$1"; exit;}
 
-kek() { \
-	whiptail --title "CRITICAL FAILURE" --msgbox "A FATAL ERROR HAS OCCURED DURING THE INSTALLATION PROCESS! AFTER CLOSING THIS WINDOW, THE ROOT DIRECTORY WILL BE WIPED. ERROR CODE: INSTLG3NTO0 " 10 60
-}
 finishmsg() { \
 	whiptail --title "Installation Complete!" --msgbox "Just kidding lel. Thank you for installing MCAFTACIP! \\n\\n To start your display server, type the command startx into the console. \\n\\n If you are using dwm, press Mod+Shift+F1 to open up the help screen." 11 90
 	}
 
 # Execute (in this order)
 welcomemsg
-licensemsg
 getuser 
 choosedotfiles 
 installdotfiles 
 chooseprograms
 installprograms
-choosesudopasswd
-setsudopasswd
-kek
 finishmsg
 
 clear
--- .scripts/screenfetch.sh
+++ .scripts/screenfetch.sh
@@ -15,32 +15,49 @@ figlet "Screenfetch"
 echo "_________________________________________________"
 echo
 
-#check status
+#display the things
+dte="$(date +"%A, %B %d | %l:%M%p")"
+echo "$dte"
+echo
+
 #awk '/open/ { print "Laptop lid is open" }' /proc/acpi/button/lid/LID/state
 #awk '/closed/ { print "Laptop lid is closed " }' /proc/acpi/button/lid/LID/state
 #awk '/1/ { print "Power cable is connected" }' /sys/class/power_supply/AC/online
 #awk '/0/ { print "Power cable is disconnected" }' /sys/class/power_supply/AC/online
 
-#pipe commands into awk
 uname -sr | awk '{ print "Kernel: " $1, $2 }'
-sensors | awk '/fan1/ { print "Fan speed: " $2, $3 }'
 ip link | awk '/wlp2s0/ { print "Wifi: " $9 }'
 lscpu | awk '/name/ { print "CPU: " $3, $5 }'
+echo
+
+#Display Hardware Usage
+
+read cpu a b c previdle rest < /proc/stat   #CPU
+prevtotal=$((a+b+c+previdle))
+sleep 0.5
+read cpu a b c idle rest < /proc/stat
+total=$((a+b+c+idle))
+cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
+echo "CPU Usage: $cpu%"
+
+mem=`free | awk '/Mem/ {printf "%d MiB/%d MiB\n", $3 / 1024.0, $2 / 1024.0 }'` #MEM
+echo "Memory Usage: $mem"
+
+sensors | awk '/fan1/ { print "Fan speed: " $2, $3 }' #Fan Speed
 
 #storing a command output in a variable and print it
+echo
 user=$(whoami) && echo "Username is: "$user
 host=$(uname -n) && echo "Hostname is: "$host
 
 #print visuals
 echo "_________________________________________________"
-cowsay "You know how this ends; the CIA sucks my cock. -Terry A. Davis"
-
-#cat ~/ascii.txt
 
 #end program and output
 )
 clear
-echo -e "\e[32m$output\e[0m"
+echo "$output"
+
 sleep 0.5
 done
 
