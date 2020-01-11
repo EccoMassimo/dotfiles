e78e4f249f6665ba3a4d0f86b0c6755c79e33257
massimo <massimo@sasha.localdomain>
2020-01-11 13:20:26 -0500

    modified:   .scripts/mcaftacip.sh

--- .scripts/mcaftacip.sh
+++ .scripts/mcaftacip.sh
@@ -35,7 +35,7 @@ installdotfiles() { \
 installprograms() { \
     clear
 	sudo dpkg --add-architecture i386
-	sudo apt install cowsay cmatrix libreoffice audacity irssi lynx htop ttf-mscorefonts-installer steam lolcat figlet screenkey obs-studio youtube-dl lutris gimp plank --no-install-recommends
+	sudo apt install cowsay cmatrix libreoffice audacity irssi lynx htop steam lolcat figlet screenkey obs-studio youtube-dl lutris gimp --no-install-recommends
 	cd /home/"$username"
 	mkdir .programs
 	installunchromium
