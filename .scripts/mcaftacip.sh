e27d4dfc9a089808d7bf3d056c118f5f13af6692
massimo <massimo@sasha.localdomain>
2020-01-11 12:01:44 -0500

    deleted:    .local/share/applications/chromium-devel.desktop 	modified:   .scripts/mcaftacip.sh

--- .scripts/mcaftacip.sh
+++ .scripts/mcaftacip.sh
@@ -1,55 +1,89 @@
-#!/bin/sh
-# Max's Catastrophically Automatic File Transfer and Customized Installation Protocol (MCAFTACIP)
-# by Max Amundsen <max@maxamundsen.xyz>
-# License: WTFPLv2
+#!/bin/bash
 
-# Functions
+function pause(){
+   read -p "$*"
+}
+
 welcomemsg() { \
-	whiptail --title "Welcome!" --msgbox "Thank you for downloading Max's Catastrophically Automatic File Transfer and Customized Installation Protocol. This script will install all the programs and utils you need to get Work™ done. \\n\\n-Max" 12 60
-	}
-
-choosedotfiles() { \
-	edition="$(whiptail --title "Dotfiles" --menu "Select dotfiles (config files) to import for the user:" 12 70 3 0 "Max's default configuration (recommended)." 1 "Don't import dotfiles (scary mode)." 3>&1 1>&2 2>&3 3>&1)" || error "User quit the installer."
+	clear
+    printf "Thanks for downloading Max's Catastrophically Automatic File Transfer and Customized Installation Protocol.\\n%s\\n Before the installer continues, you must input your username.\\n The script will check if this is a valid user so don't worry\\n if you typed it wrong.\\n\\n If at any point you want to exit, press [CTRL+C] to quit the installer."
+    echo
+    echo
 	}
 
 getuser() { \
-	# Prompts user for new username an password.
-	username=$(whiptail --inputbox "Please enter your username." 10 60 3>&1 1>&2 2>&3 3>&1) || error "User quit the installer."
-	while ! echo "$username" | grep '/home' /etc/passwd | cut -d: -f1 | grep -w "$username" >/dev/null 2>&1; do
-		username=$(whiptail --inputbox "Username not valid. Please enter a valid system user." 10 60 3>&1 1>&2 2>&3 3>&1) || error "User quit the installer."
-	done
-	}
+    clear
+    echo "Input username:"
+    read username
 
-installdotfiles(){ \
-if echo "$edition" | grep '0' >/dev/null 2>&1; then
+while ! echo "$username" | grep '/home' /etc/passwd | cut -d: -f1 | grep -w "$username" ; do
+    echo "Username not valid. Try again."
+    sleep 1
+    getuser
+    done
+}
+
+choosedotfiles() { \
+    clear
+    printf "Choose which dotfiles you would like to import.\\n%s 0. Max's default config\\n 1. Do not import dotfiles"
+    echo
+    echo
+    echo "Enter selection (0) or (1) :"
+    read dotfiles
+    installdotfiles
+}
+
+installdotfiles() { \
+if ! echo "$dotfiles" | grep -E '(0|1)' ; then
+   echo "Option not valid. Try again."
+   sleep 1
+   choosedotfiles
+   fi
+
+if echo "$dotfiles" | grep '0' ; then
+    clear
 	cd /home/"$username"
 	git clone https://github.com/eccomassimo/dotfiles.git
-	#cd /home/"$username"/dotfiles
-	mv /home/"$username"/dotfiles/* /home/"$username"/dotfiles/.* /home/"$username"/
+	mv -f /home/"$username"/dotfiles/* /home/"$username"/dotfiles/.* /home/"$username"/
 	cd /home/"$username"
 	rm -rf dotfiles/
 	fi
 
-if echo "$edition" | grep '1' >/dev/null 2>&1; then
+if echo "$dotfiles" | grep '1' ; then
 	cd /home/"$username"
+    clear
 	fi
 }
 
-chooseprograms(){ \
-	programs="$(whiptail --title "Programs" --menu "Select Programs to install:" 10 80 2 0 "Everything you need to get started (recommended)." 1 "None (Don't install anything)." 3>&1 1>&2 2>&3 3>&1)" || error "User quit the installer."	
+chooseprograms() { \
+    clear
+    printf "Would you like to install additional programs?\\n%s 0. Everything you need to get started\\n 1. Do not install additional programs." 
+    echo
+    echo
+    echo "Enter selection (0) or (1) :"
+    read programs
+    installprograms
 }
 
-installprograms(){ \
-if echo "$programs" | grep '0' >/dev/null 2>&1; then
+installprograms() { \
+if ! echo "$programs" | grep -E '(0|1)' ; then
+   echo "Option not valid. Try again."
+   sleep 1
+   chooseprograms
+   fi
+
+if echo "$programs" | grep '0' ; then
+    clear
 	sudo dpkg --add-architecture i386
-	sudo apt install cowsay cmatrix libreoffice audacity irssi lynx htop ttf-mscorefonts-installer steam lolcat figlet screenkey obs-studio youtube-dl lutris gimp
+	sudo apt install cowsay cmatrix libreoffice audacity irssi lynx htop ttf-mscorefonts-installer steam lolcat figlet screenkey obs-studio youtube-dl lutris gimp plank
 	cd /home/"$username"
 	mkdir .programs
 	installunchromium
 	fi
 
-if echo "$programs" | grep '1' >/dev/null 2>&1; then
-	echo "nope"
+if echo "$programs" | grep '1' ; then
+    clear
+	echo "No programs installed."
 	fi
 }
 
@@ -60,27 +94,36 @@ installunchromium(){ \
 	rm ungoogled-chromium_78.0.3904.108-2.1_linux.tar.xz
 	sleep .5
 	cd /home/"$username"/.programs/ungoogled-chromium_78.0.3904.108-2.1_linux
-	mv chrome_sandbox chrome-sandbox
+	mv -f chrome_sandbox chrome-sandbox
 	sudo chmod 4755 /home/"$username"/.programs/ungoogled-chromium_78.0.3904.108-2.1_linux/chrome-sandbox
 	sudo chown root /home/"$username"/.programs/ungoogled-chromium_78.0.3904.108-2.1_linux/chrome-sandbox
-	
 
+    echo "[Desktop Entry]
+Version=1.0
+Encoding=UTF-8
+Name=Ungoogled Chromium
+Exec=/home/$username/.programs/ungoogled-chromium_78.0.3904.108-2.1_linux/chrome-wrapper %U
+Terminal=false
+Icon=/home/$username/.programs/ungoogled-chromium_78.0.3904.108-2.1_linux/icon.png
+Type=Application
+Categories=Application;Network;WebBrowser;
+MimeType=x-scheme-handler/unknown;x-scheme-handler/about;x-scheme-handler/https;x-scheme-handler/http;text/html;text/xml;application/xhtml_xml;" > /home/$username/.local/share/applications/chromium-devel.desktop
 }
 
-error() { clear; printf "Installation could not complete:\\n%s\\n" "$1"; exit;}
-
-finishmsg() { \
-	whiptail --title "Installation Complete!" --msgbox "Just kidding lel. Thank you for installing MCAFTACIP! \\n\\n To start your display server, type the command startx into the console. \\n\\n If you are using dwm, press Mod+Shift+F1 to open up the help screen." 11 90
+exitmsg() { \
+	clear
+    echo "done = good;"
+    echo
+    echo
 	}
 
-# Execute (in this order)
+# Things that are executed (in order)
 welcomemsg
-getuser 
-choosedotfiles 
-installdotfiles 
+pause "Press [Return] key to continue..."
+getuser
+choosedotfiles
 chooseprograms
-installprograms
-finishmsg
-
+exitmsg
+pause "Press [Return] key to continue..."
 clear
-echo "done = good;"
+
