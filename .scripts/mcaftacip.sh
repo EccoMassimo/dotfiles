df121eb684cce009c80d130db6496cc4cd778794
massimo <massimo@sasha.localdomain>
2020-01-18 22:08:14 -0500

    modified:   .config/VSCodium/User/settings.json 	modified:   .scripts/mcaftacip.sh

--- .scripts/mcaftacip.sh
+++ .scripts/mcaftacip.sh
@@ -9,52 +9,39 @@ welcomemsg() { \
     echo "THIS SCRIPT CAN NOT BE RUN AS ROOT. IF YOU DID GO BACK NOW
 
 Thank you for using Max's Catastrophically Automatic File Transfer and Customized Installation Protocol.
-Before the installer continues, you must input your username.
+Before the installer continues, you must input your USER.
 The script will check if this is a valid user so don't worry if you typed it wrong.
 If at any point you want to exit, press [CTRL+C] to quit the installer."
     echo
     echo
 	}
 
-getuser() { \
-    clear
-    echo "Input username:"
-    read username
-
-while ! echo "$username" | grep '/home' /etc/passwd | cut -d: -f1 | grep -w "$username" >/dev/null ; do
-    echo "Username not valid. Try again."
-    sleep 1
-    getuser
-    done
-}
-
 choosedotfiles() { \
 	clear
-	read -e -p "Would you like to clone the dotfiles repo? (y/n)" choice
+	read -e -p "Would you like to install Massimo's custom settings? (y/n)   " choice
 	[[ "$choice" == [Yy]* ]] && echo "Cloning dotfiles..." && installdotfiles
 	
 }
 
-
 installdotfiles() { \
     clear
-	cd /home/"$username"/
+	cd /home/"$USER"/
 	git clone https://github.com/eccomassimo/dotfiles.git
-	mv -f /home/"$username"/dotfiles/* /home/"$username"/dotfiles/.* /home/"$username"/
-	cd /home/"$username"/
+	mv -f /home/"$USER"/dotfiles/* /home/"$USER"/dotfiles/.* /home/"$USER"/
+	cd /home/"$USER"/
 	rm -rf dotfiles/
 }
 
 chooseprograms() { \
 	# prompt to install ungoogled chromium
 	clear
-	read -e -p "Would you like to install Ungoogled Chromium? (y/n)" choice
+	read -e -p "Would you like to install Ungoogled Chromium? (y/n) " choice
 	[[ "$choice" == [Yy]* ]] && echo "Installing Ungoogled Chromium..." && installunchromium
 
 	# prompt to install vscodium & frc tools
 	clear
-	read -e -p "Would you like to install VSCodium? (y/n)" choice
-	[[ "$choice" == [Yy]* ]] && echo "Installing VSCodium..." && installvscodium && read -e -p "Would you like to install WPILib for FRC? (y/n)" choice
+	read -e -p "Would you like to install VSCodium? (y/n) " choice
+	[[ "$choice" == [Yy]* ]] && echo "Installing VSCodium..." && installvscodium && read -e -p "Would you like to install WPILib for FRC? (y/n) " choice
 	[[ "$choice" == [Yy]* ]] && echo "Installing FRC Tools..." && installwpilib
 }
 
@@ -64,57 +51,125 @@ installprograms() { \
 	sudo apt install cowsay cmatrix libreoffice audacity irssi lynx htop steam lolcat figlet screenkey obs-studio youtube-dl lutris gimp gnome-tweaks exfat-utils exfat-fuse openjdk-8-jre openjdk-11-jre openjdk-8-jdk openjdk-11-jdk playonlinux virtualbox gnome-shell-extension-dashtodock gnome-shell-extension-multi-monitors gnome-shell-extension-no-annoyance mlocate
 	sudo apt remove firefox
 	sudo apt autoremove
-	cd /home/"$username"/
+	cd /home/"$USER"/
+	mkdir .programs
+	clear
 }
 
 installunchromium(){ \
-	cd /home/"$username"/.programs
+	cd /home/"$USER"/.programs
 	wget https://github.com/Eloston/ungoogled-chromium-binaries/releases/download/78.0.3904.108-2.1/ungoogled-chromium_78.0.3904.108-2.1_linux.tar.xz
 	tar -xf ungoogled-chromium_78.0.3904.108-2.1_linux.tar.xz
 	rm ungoogled-chromium_78.0.3904.108-2.1_linux.tar.xz
 	sleep .5
-	cd /home/"$username"/.programs/ungoogled-chromium_78.0.3904.108-2.1_linux
+	cd /home/"$USER"/.programs/ungoogled-chromium_78.0.3904.108-2.1_linux
 	mv -f chrome_sandbox chrome-sandbox
-	sudo chmod 4755 /home/"$username"/.programs/ungoogled-chromium_78.0.3904.108-2.1_linux/chrome-sandbox
-	sudo chown root /home/"$username"/.programs/ungoogled-chromium_78.0.3904.108-2.1_linux/chrome-sandbox
+	sudo chmod 4755 /home/"$USER"/.programs/ungoogled-chromium_78.0.3904.108-2.1_linux/chrome-sandbox
+	sudo chown root /home/"$USER"/.programs/ungoogled-chromium_78.0.3904.108-2.1_linux/chrome-sandbox
 
-    touch /home/$username/.local/share/applications/chromium-devel.desktop
+    touch /home/$USER/.local/share/applications/chromium-devel.desktop
     echo "[Desktop Entry]
 Version=1.0
 Encoding=UTF-8
 Name=Ungoogled Chromium
-Exec=/home/$username/.programs/ungoogled-chromium_78.0.3904.108-2.1_linux/chrome-wrapper %U
+Exec=/home/$USER/.programs/ungoogled-chromium_78.0.3904.108-2.1_linux/chrome-wrapper %U
 Terminal=false
-Icon=/home/$username/.programs/ungoogled-chromium_78.0.3904.108-2.1_linux/icon.png
+Icon=/home/$USER/.programs/ungoogled-chromium_78.0.3904.108-2.1_linux/icon.png
 Type=Application
 Categories=Application;Network;WebBrowser;
-MimeType=x-scheme-handler/unknown;x-scheme-handler/about;x-scheme-handler/https;x-scheme-handler/http;text/html;text/xml;application/xhtml_xml;" > /home/$username/.local/share/applications/chromium-devel.desktop
+MimeType=x-scheme-handler/unknown;x-scheme-handler/about;x-scheme-handler/https;x-scheme-handler/http;text/html;text/xml;application/xhtml_xml;" > /home/$USER/.local/share/applications/chromium-devel.desktop
 }
 
 installvscodium() { \
-	cd /home/"$username"/
-	mkdir .programs 
-	cd /home/"$username"/.programs
+	cd /home/"$USER"/.programs
 	wget https://github.com/VSCodium/vscodium/releases/download/1.41.1/codium_1.41.1-1576787344_amd64.deb
 	sudo dpkg -i codium_1.41.1-1576787344_amd64.deb
 	rm -f codium_1.41.1-1576787344_amd64.deb
-	codium --install-extension visualstudioexptteam.vscodeintellicode eamodio.gitlens vscjava.vscode-java-pack siddharthkp.codesandbox-black zhuangtongfa.material-theme
+	codium --install-extension vscjava.vscode-java-pack 
+	codium --install-extension zhuangtongfa.material-theme
 	clear
 }
 
 installwpilib() { \
-	cd /home/"$username"/
-	mkdir wpilib
-	cd wpilib
-	mkdir 2020
-	wget https://github.com/wpilibsuite/allwpilib/releases/download/v2020.1.2/WPILib_Linux-2020.1.2.tar.gz
-	tar -xf WPILib_Linux-2020.1.2.tar.gz
-	sudo rm -rf WPILib_Linux-2020.1.2.tar.gz
-	cd vsCodeExtensions
-	codium --install-extension JavaDebug.vsix JavaDeps.vsix JavaLang.vsix WPILib.vsix
-	clear
-}
+	mkdir -p /home/"$USER"/wpilib/2020
+	cd /home/"$USER"/wpilib/2020
+	wget --output-document=WPILib_Linux.tar.gz `curl -s https://api.github.com/repos/wpilibsuite/allwpilib/releases | grep browser_download_url | grep 'Linux' | head -n 1 | cut -d '"' -f 4`
+	tar -xf WPILib_Linux.tar.gz
+	rm -rf WPILib_Linux.tar.gz
+	cd /home/"$USER"/wpilib/2020/vsCodeExtensions
+	codium --install-extension JavaDebug.vsix 
+	codium --install-extension JavaDeps.vsix 
+	codium --install-extension JavaLang.vsix 
+	codium --install-extension WPILib.vsix
+	cd /home/"$USER"/wpilib/2020/tools
+	java -jar ToolsUpdater.jar
+	#Set Desktop Entries
+	touch /home/$USER/.local/share/applications/pathweaver.desktop
+	echo "[Desktop Entry]
+Version=2020
+Encoding=UTF-8
+Name=FRC PathWeaver
+Exec=java -jar /home/$USER/wpilib/2020/tools/PathWeaver.jar
+Terminal=false
+Icon=/home/$USER/.programs/wpilib/icon.png
+Type=Application
+Categories=Application;FRC;WPILib;" > /home/$USER/.local/share/applications/pathweaver.desktop
 
+	touch /home/$USER/.local/share/applications/shuffleboard.desktop
+	echo "[Desktop Entry]
+Version=2020
+Encoding=UTF-8
+Name=FRC ShuffleBoard
+Exec=java -jar /home/$USER/wpilib/2020/tools/shuffleboard.jar
+Terminal=false
+Icon=/home/$USER/.programs/wpilib/icon.png
+Type=Application
+Categories=Application;FRC;WPILib;" > /home/$USER/.local/share/applications/shuffleboard.desktop
+
+	touch /home/$USER/.local/share/applications/outlineviewer.desktop
+	echo "[Desktop Entry]
+Version=2020
+Encoding=UTF-8
+Name=FRC OutlineViewer
+Exec=java -jar /home/$USER/wpilib/2020/tools/OutlineViewer.jar
+Terminal=false
+Icon=/home/$USER/.programs/wpilib/icon.png
+Type=Application
+Categories=Application;FRC;WPILib;" > /home/$USER/.local/share/applications/outlineviewer.desktop
+
+	touch /home/$USER/.local/share/applications/robotbuilder.desktop
+	echo "[Desktop Entry]
+Version=2020
+Encoding=UTF-8
+Name=FRC RobotBuilder
+Exec=java -jar /home/$USER/wpilib/2020/tools/RobotBuilder.jar
+Terminal=false
+Icon=/home/$USER/.programs/wpilib/icon.png
+Type=Application
+Categories=Application;FRC;WPILib;" > /home/$USER/.local/share/applications/robotbuilder.desktop
+
+	touch /home/$USER/.local/share/applications/smartdashboard.desktop
+	echo "[Desktop Entry]
+Version=2020
+Encoding=UTF-8
+Name=FRC SmartDashboard
+Exec=java -jar /home/$USER/wpilib/2020/tools/SmartDashboard.jar
+Terminal=false
+Icon=/home/$USER/.programs/wpilib/icon.png
+Type=Application
+Categories=Application;FRC;WPILib;" > /home/$USER/.local/share/applications/smartdashboard.desktop
+
+	touch /home/$USER/.local/share/applications/toolsupdater.desktop
+	echo "[Desktop Entry]
+Version=2020
+Encoding=UTF-8
+Name=FRC ToolsUpdater
+Exec=java -jar /home/$USER/wpilib/2020/tools/ToolsUpdater.jar
+Terminal=false
+Icon=/home/$USER/.programs/wpilib/icon.png
+Type=Application
+Categories=Application;FRC;WPILib;" > /home/$USER/.local/share/applications/toolsupdater.desktop
+}
 
 exitmsg() { \
 	clear
@@ -128,7 +183,6 @@ exitmsg() { \
 # Things that are executed (in order)
 welcomemsg
 pause "Press [Return] key to continue..."
-getuser
 choosedotfiles
 chooseprograms
 exitmsg
